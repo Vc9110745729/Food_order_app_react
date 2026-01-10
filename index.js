11553f57b5b23f8f3c2b529e5ba8c9e02bba47ea
Chethan shetty <chethanv997@gmail.com>
2026-01-10 22:06:28 +0530

    This is the 4th episode 1st day of building my project

--- index.js
+++ index.js
@@ -1,3 +1,473 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./styles.css";
 
+/*
+STRUCTURE IS - 
+Header
+    Logo
+    Nav-links
+Body
+    search
+    Card-container
+        resto-cards
+          Image, name, cuisines, Star_rating, deliviry time 
+Footer
+    copy right
+    links
+    contact us
+*/
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo">
+        <img
+          src="https://media.istockphoto.com/id/981368726/vector/restaurant-food-drinks-logo-fork-knife-background-vector-image.jpg?s=612x612&w=0&k=20&c=9M26CBkCyEBqUPs3Ls5QCjYLZrB9sxwrSFmnAmNCopI="
+          alt="logo"
+        />
+      </div>
+      <div className="nav_items">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>Contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Restaurant_cards = (props) => {
+  const { resData } = props;
+  const { cloudinaryImageId, name, cuisines, avgRating, costForTwo, sla } =
+    resData?.info;
+  console.log(props);
+
+  return (
+    <div className="restaurant-cards">
+      <img
+        className="res-image"
+        alt="This is a pizzas image"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +
+          cloudinaryImageId
+        }
+      />
+      <div className="res-details">
+        <h3>{name}</h3>
+        {/* <p>{cuisines}</p> */}
+        <p>{cuisines.join(", ")}</p>
+        <p>{avgRating}</p>
+        <p> Cost : {costForTwo}</p>
+        <p>Deliviry time : {sla.slaString} minutes </p>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        {/* <Restaurant_cards resData={resList[0]} />
+        <Restaurant_cards resData={resList[1]} />
+        <Restaurant_cards resData={resList[2]} />
+        <Restaurant_cards resData={resList[3]} /> 
+        <Restaurant_cards resData={resList[4]} /> */}
+        {resList.map((restaurant, index) => (
+          <Restaurant_cards key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
+
+const resList = [
+  {
+    info: {
+      id: "17304",
+      name: "KFC",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2026/1/8/9cab1c1a-1a57-474a-8512-013b5dbafc96_17304.JPG",
+      locality: "Ganganagar",
+      areaName: "R T Nagar",
+      costForTwo: "₹400 for two",
+      cuisines: ["Burgers", "Fast Food", "Rolls & Wraps"],
+      avgRating: 4,
+      parentId: "547",
+      avgRatingString: "4.0",
+      totalRatingsString: "24K+",
+      sla: {
+        deliveryTime: 36,
+        lastMileTravel: 3,
+        serviceability: "SERVICEABLE",
+        slaString: "35-40 mins",
+        lastMileTravelString: "3.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-01-11 01:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "23796",
+      name: "Domino's Pizza",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/11/11/75855305-76ff-4551-affd-d2f0c58ddd35_23796.JPG",
+      locality: "Cunningham Road",
+      areaName: "Central Bangalore",
+      costForTwo: "₹400 for two",
+      cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
+      avgRating: 4.4,
+      parentId: "2456",
+      avgRatingString: "4.4",
+      totalRatingsString: "18K+",
+      sla: {
+        deliveryTime: 20,
+        lastMileTravel: 5,
+        serviceability: "SERVICEABLE",
+        slaString: "15-20 mins",
+        lastMileTravelString: "5.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-01-10 23:59:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Pizza.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Pizza.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "50% OFF",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+  },
+  {
+    info: {
+      id: "30083",
+      name: "California Burrito",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/9/3/d975fca2-038b-43e6-9c72-31d7113c8ae0_30083.jpg",
+      locality: "Sadashivanagar ",
+      areaName: "sadashiva nagar ",
+      costForTwo: "₹250 for two",
+      cuisines: [
+        "Mexican",
+        "American",
+        "Salads",
+        "Continental",
+        "Keto",
+        "Healthy Food",
+        "Beverages",
+        "Snacks",
+        "Desserts",
+        "Fast Food",
+      ],
+      avgRating: 4.6,
+      parentId: "1252",
+      avgRatingString: "4.6",
+      totalRatingsString: "27K+",
+      sla: {
+        deliveryTime: 32,
+        lastMileTravel: 2.8,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "2.8 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-01-11 01:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "High%20Protein/rx%20tag%205.png",
+            description: "High Protein",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "High Protein",
+                  imageId: "High%20Protein/rx%20tag%205.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹79",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.5",
+          ratingCount: "556",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-1e2b8efa-128c-4d05-93ba-842cec79526a",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/california-burrito-sadashivanagar-sadashiva-nagar-rest30083",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "333153",
+      name: "Baskin Robbins - Ice Cream Desserts",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/4/24/6b6c778b-c9bf-4027-9541-ca1ff970cead_333153.JPG",
+      locality: "Dinnur Main Road",
+      areaName: "R T Nagar",
+      costForTwo: "₹250 for two",
+      cuisines: ["Desserts", "Ice Cream"],
+      avgRating: 4.6,
+      veg: true,
+      parentId: "5588",
+      avgRatingString: "4.6",
+      totalRatingsString: "835",
+      sla: {
+        deliveryTime: 32,
+        lastMileTravel: 3,
+        serviceability: "SERVICEABLE",
+        slaString: "30-35 mins",
+        lastMileTravelString: "3.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-01-11 01:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹51",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-1e2b8efa-128c-4d05-93ba-842cec79526a",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/baskin-robbins-ice-cream-desserts-dinnur-main-road-r-t-nagar-rest333153",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "17589",
+      name: "Polar Bear",
+      cloudinaryImageId: "57262fe3839f0bff174f3d7e7cc8a2b4",
+      locality: "Deve Gowda Nagar",
+      areaName: "R.T. Nagar",
+      costForTwo: "₹300 for two",
+      cuisines: ["Ice Cream", "Desserts"],
+      avgRating: 4.6,
+      veg: true,
+      parentId: "726",
+      avgRatingString: "4.6",
+      totalRatingsString: "11K+",
+      sla: {
+        deliveryTime: 29,
+        lastMileTravel: 2.5,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "2.5 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2026-01-10 23:59:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Ratnesh_Badges/Rx_Awards_2025/Icecream.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Ratnesh_Badges/Rx_Awards_2025/Icecream.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-1e2b8efa-128c-4d05-93ba-842cec79526a",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/bangalore/polar-bear-deve-gowda-nagar-r-t-nagar-rest17589",
+      type: "WEBLINK",
+    },
+  },
+];
